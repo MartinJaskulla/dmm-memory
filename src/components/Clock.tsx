a1f64833208918d70a02e0eb123a3c3318e239e4
Martin Jaskulla <35801656+MartinJaskulla@users.noreply.github.com>
2022-06-01 11:36:02 +0200

    style(Clock): Remove eslint-disable-next-line react-hooks/exhaustive-deps

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { useGame } from '../features/useGame';
+import { Move, useGame } from '../features/useGame';
 import { interval } from '../utils/interval';
 
 // Put clock in its own component instead of game context
@@ -10,12 +10,11 @@ export function Clock() {
   const [seconds, setSeconds] = useState(0);
 
   const abortControllerRef = useRef(new AbortController());
-  useEffect(() => () => abortControllerRef.current.abort(), []);
 
   useEffect(() => {
     abortControllerRef.current.abort();
     setSeconds(new Date(game.history.move.totalMs).getUTCSeconds());
-    start(game.history.move.totalMs);
+    start(game.history.move, abortControllerRef, setSeconds);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [game.history.timeTravels]);
 
@@ -23,21 +22,27 @@ export function Clock() {
     if (game.history.move.gameOver) abortControllerRef.current.abort();
   }, [game.history.move.gameOver]);
 
-  function start(startingMs: number) {
-    if (game.history.move.gameOver) return;
-    abortControllerRef.current = new AbortController();
-    const msToNextSecond = 1000 - (startingMs % 1000);
-    // When traveling back in time to a move which was made 7.3s in, the clock needs to be updated in 0.7s.
-    interval(msToNextSecond, abortControllerRef.current.signal, () => {
-      abortControllerRef.current.abort();
-      abortControllerRef.current = new AbortController();
-      setSeconds((seconds) => seconds + 1);
-      // Now we can update every second
-      interval(1000, abortControllerRef.current.signal, () => setSeconds((seconds) => seconds + 1));
-    });
-  }
+  useEffect(() => () => abortControllerRef.current.abort(), []);
 
   const formattedSeconds = new Date(seconds * 1000).toISOString().slice(14, 19);
 
   return <span>Time: {formattedSeconds}</span>;
+}
+
+function start(
+  move: Move,
+  abortControllerRef: React.MutableRefObject<AbortController>,
+  setSeconds: React.Dispatch<React.SetStateAction<number>>,
+) {
+  if (move.gameOver) return;
+  abortControllerRef.current = new AbortController();
+  const msToNextSecond = 1000 - (move.totalMs % 1000);
+  // When traveling back in time to a move which was made 7.3s in, the clock needs to be updated in 0.7s.
+  interval(msToNextSecond, abortControllerRef.current.signal, () => {
+    abortControllerRef.current.abort();
+    abortControllerRef.current = new AbortController();
+    setSeconds((seconds) => seconds + 1);
+    // Now we can update every second
+    interval(1000, abortControllerRef.current.signal, () => setSeconds((seconds) => seconds + 1));
+  });
 }
