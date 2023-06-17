0b78a7a980190d48ca4a09b5abf40b6f9b85856d
MartinJaskulla <martinjaskulla@icloud.com>
2023-06-17 12:54:37 +0900

    3D card flip

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,6 +6,7 @@ import { MatchedCard } from './Cards/MatchedCard';
 import styled from 'styled-components';
 import { useHistory } from '../hooks/useHistory/useHistory';
 import { useGame } from '../hooks/useGame/useGame';
+import { Card } from './Cards/Card';
 
 const StyledDiv = styled.div`
   display: grid;
@@ -36,35 +37,51 @@ export function Board() {
           const card = history.move.cards[cardId];
           const hasHighlight = history.move.highlighted.has(cardId) && !history.move.gameOver;
 
+          let back;
+          let flipped = false;
+
           if (card.type === 'matchable') {
-            if (history.move.matched.has(card.cardId)) {
-              return (
-                <MatchedCard key={card.cardId} lang={card.language} highlight={hasHighlight}>
-                  {card.text}
-                </MatchedCard>
-              );
-            }
+            const isMatch = history.move.matched.has(card.cardId);
+            back = isMatch ? (
+              <MatchedCard lang={card.language} highlight={hasHighlight}>
+                {card.text}
+              </MatchedCard>
+            ) : (
+              <RevealedCard lang={card.language} highlight={hasHighlight}>
+                {card.text}
+              </RevealedCard>
+            );
 
             const isCardRevealed = [history.move.choice1, history.move.choice2].includes(card.cardId);
             const isHintCard = history.move.hinted.has(card.cardId);
-            if (isCardRevealed || isHintCard || history.move.gameOver) {
-              return (
-                <RevealedCard key={card.cardId} lang={card.language} highlight={hasHighlight}>
-                  {card.text}
-                </RevealedCard>
-              );
+            if (isCardRevealed || isHintCard || isMatch || history.move.gameOver) {
+              flipped = true;
             }
-          }
-
-          if (card.type === 'effect' && (history.move.foundEffects.has(card.cardId) || history.move.gameOver)) {
-            return (
+          } else if (card.type === 'effect') {
+            back = (
               <EffectCard key={card.cardId} highlight={hasHighlight}>
                 {card.text}
               </EffectCard>
             );
+            if (history.move.foundEffects.has(card.cardId) || history.move.gameOver) {
+              flipped = true;
+            }
           }
 
-          return <HiddenCard key={card.cardId} onClick={() => game.revealCard(index)} highlight={hasHighlight} />;
+          return (
+            <Card
+              key={card.cardId}
+              flipped={flipped}
+              front={
+                <HiddenCard
+                  key={card.cardId}
+                  onClick={() => !flipped && game.revealCard(index)}
+                  highlight={hasHighlight}
+                />
+              }
+              back={back}
+            />
+          );
         })}
       </StyledMain>
     </StyledDiv>
--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,60 +1,61 @@
-import React, {ReactNode} from 'react';
-import {useState} from 'react';
-import {useSpring, animated} from 'react-spring';
+import React, { ReactNode } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 // TODO Remove ts-ignore
-
-interface CardArgs {
-    front: ReactNode;
-    back: ReactNode;
-}
 // TODO Issue is the hover effect. it is only applied of the card is interactive e.g. effect card cannot be turned back.
 
-// React spring animations are interruptible, which is nice if you flip 3rd card 2nd animations stops
-export function Card({front, back}: CardArgs) {
-    // flipped as input
-    const [flipped, set] = useState(false);
-    const {transform, opacity} = useSpring({
-        opacity: flipped ? 1 : 0,
-        transform: `perspective(3000px) rotateY(${flipped ? 180 : 0}deg)`,
-        config: {mass: 5, tension: 500, friction: 80},
-    });
-    return (
-        <div
-            onClick={() => set((state) => !state)}
-            style={{
-                position: 'relative',
-                width: '100%',
-                height: '100%',
-            }}
-        >
-            {/* @ts-ignore */}
-            <animated.div
-                style={{
-                    backfaceVisibility: 'hidden',
-                    willChange: 'opacity',
-                    opacity: opacity.interpolate((o) => 1 - o),
-                    transform,
-                    position: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                }}
-            >
-                {front}
-            </animated.div>
-            <animated.div
-                style={{
-                    backfaceVisibility: 'hidden',
-                    willChange: 'opacity',
-                    opacity,
-                    transform: transform.interpolate((t) => `${t} rotateY(180deg)`),
-                    position: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                }}
-            >
-                {back}
-            </animated.div>
-        </div>
-    );
+interface CardArgs {
+  front: ReactNode;
+  back: ReactNode;
+  flipped: boolean;
+}
+
+// React spring animations are interruptible. For example:
+// - Flip card 1
+// - Flip non-matching card 2
+// - Flip card 3. Card 1 and 2 are un-flipped. If the animation of card 2 has not finished,
+// it will stop in-place and smoothly transition to the reverse un-flipping animation.
+export function Card({ front, back, flipped }: CardArgs) {
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(3000px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+  return (
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      {/* @ts-ignore */}
+      <animated.div
+        style={{
+          backfaceVisibility: 'hidden',
+          willChange: 'opacity',
+          opacity: opacity.interpolate((o) => 1 - o),
+          transform,
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {front}
+      </animated.div>
+      <animated.div
+        style={{
+          backfaceVisibility: 'hidden',
+          willChange: 'opacity',
+          opacity,
+          transform: transform.interpolate((t) => `${t} rotateY(180deg)`),
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {back}
+      </animated.div>
+    </div>
+  );
 }
